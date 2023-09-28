0b5983bfc5c3b8349486c5eb1405b74cfdafa2bc
Ilya Kochura <ikochura89@gmail.com>
2023-09-28 15:23:57 +0300

    Remove debug Gui fps counter

--- Assets/Scripts/Initialize/Initialize.cs
+++ Assets/Scripts/Initialize/Initialize.cs
@@ -5,16 +5,9 @@ namespace Initialize
 {
     public class Initialize : MonoBehaviour
     {
-        private float _fps;
-        
         private void Awake()
         {
             ObjectPool.CreateInstance();
-        }
-        void OnGUI()
-        {
-            _fps = 1.0f / Time.deltaTime;
-            GUILayout.Label("FPS: " + (int)_fps);
         }
     }
 }
